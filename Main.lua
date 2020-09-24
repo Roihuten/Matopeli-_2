0940a028ee050ed2fdedf231deda77bbdc426ddb
Robertsu24 <santeri.ristonen@gmail.com>
2020-09-24 18:19:07 +0300

    Mato liikkuu ees taas

--- Main.lua
+++ Main.lua
@@ -12,15 +12,16 @@ end
 function reset()
 
 	mato ={
-	{x = 3, y = 1},
-	{x = 2, y = 1},
-	{x = 1, y = 1},
+	{x = 3+leveys/2, y = korkeus/2},
+	{x = 2+leveys/2, y = korkeus/2},
+	{x = 1+leveys/2, y = korkeus/2},
 	}
 	
 	suunta = 'right'
 	ajastin = 0
 	
 end
+
 
 
 -- Funktio peli"framien" piirtämiseen
@@ -33,4 +34,42 @@ function love.draw()
 	local function piirraRuutu(x,y)
 		love.graphics.rectangle('fill',(x-1)*ruutu,(y-1)*ruutu,ruutu-1,ruutu-1)
 	end
+	
+	for indeksi , matopala in ipairs(mato) do
+		love.graphics.setColor(.6,.9,0.32)
+		piirraRuutu(matopala.x, matopala.y)
+	end
+	
 end
+
+function love.update(dt)
+	
+	local seuraavax = mato[1].x
+	local seuraavay = mato[1].y
+	
+	if suunta == 'right' then
+		seuraavax = seuraavax + 1
+		
+		if seuraavax > leveys - 1 then
+		suunta = 'left'
+		end
+		
+	elseif suunta == 'left' then
+		seuraavax = seuraavax - 1
+		
+		if seuraavax < 1 then
+		suunta = 'right'
+		end
+	end
+	
+	table.insert(mato,1,{x =seuraavax, y = seuraavay})
+	table.remove(mato)
+end
+
+
+
+
+
+
+
+
